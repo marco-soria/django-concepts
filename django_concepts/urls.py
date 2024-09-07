a3b1d5c8794c2c550484324b0a285e12f561d3fd
marco-soria <marco.soriaa7@gmail.com>
2024-09-07 16:30:19 -0500

    added view in classes

--- django_concepts/urls.py
+++ django_concepts/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from my_first_app.views import my_view
+from django.urls import include, path
+#from my_first_app.views import my_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('car_list/', my_view),
+    path('cars/', include('my_first_app.urls')),
 ]
